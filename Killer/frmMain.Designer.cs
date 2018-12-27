0c1cb2cbc1a9f666e053836cce1ccccfe42dc8ff
SatellaSoft <satellasoft@gmail.com>
2018-12-27 10:29:08 -0200

    First Commit

---
+++ Killer/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Killer
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			this.panelBottom = new System.Windows.Forms.Panel();
+			this.panelLeft = new System.Windows.Forms.Panel();
+			this.panelRight = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// panelBottom
+			// 
+			this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelBottom.Location = new System.Drawing.Point(0, 353);
+			this.panelBottom.Name = "panelBottom";
+			this.panelBottom.Size = new System.Drawing.Size(782, 100);
+			this.panelBottom.TabIndex = 0;
+			// 
+			// panelLeft
+			// 
+			this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panelLeft.Location = new System.Drawing.Point(0, 0);
+			this.panelLeft.Name = "panelLeft";
+			this.panelLeft.Size = new System.Drawing.Size(385, 353);
+			this.panelLeft.TabIndex = 1;
+			// 
+			// panelRight
+			// 
+			this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panelRight.Location = new System.Drawing.Point(397, 0);
+			this.panelRight.Name = "panelRight";
+			this.panelRight.Size = new System.Drawing.Size(385, 353);
+			this.panelRight.TabIndex = 3;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(782, 453);
+			this.Controls.Add(this.panelRight);
+			this.Controls.Add(this.panelLeft);
+			this.Controls.Add(this.panelBottom);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Kill";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panelBottom;
+		private System.Windows.Forms.Panel panelLeft;
+		private System.Windows.Forms.Panel panelRight;
+	}
+}
+
